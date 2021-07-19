172c8619ad5b364e6da20a02d47e8b093d5874c2
김현수 <2019023436@hanyang.ac.kr>
2021-07-19 13:36:16 +0900

    removed unnecessary code

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from madmom.features import *
 
 
-def get_notes():
-    proc = CNNPianoNoteProcessor()
-    adsr = ADSRNoteTrackingProcessor(pitch_offset=21, onset_threshold=0.9)
-
-    audio_file = 'data/youngPlane.m4a'
-    act = proc(audio_file)
-    MIDI_notes = adsr(act)
-
-    print("get")
-    print(MIDI_notes)
-
-    return MIDI_notes
-
-
-def post_notes(file):
+def audio_to_MIDI_notes(file):
     proc = CNNPianoNoteProcessor()
     adsr = ADSRNoteTrackingProcessor(pitch_offset=21, onset_threshold=0.9)
 
--- server.py
+++ server.py
@@ -1,19 +1,12 @@
 import os
 from flask_cors import CORS
 from flask import Flask, jsonify, request
-from flask_restx import Resource, Api, reqparse
 import main
-from madmom.features import *
+
 
 app = Flask(__name__)
-api = Api(app)
 app.config['DEBUG'] = True
 CORS(app)
-
-
-@app.route('/ddd')
-def index():
-    return 'Hello world'
 
 
 @app.route('/express_to_flask', methods=['POST'])
@@ -26,7 +19,7 @@ def express_to_flask():
     saved_file_path = os.path.join(dir_path, fileName)
     parsed_request.save(saved_file_path)
 
-    notes = main.post_notes(saved_file_path)
+    notes = main.audio_to_MIDI_notes(saved_file_path)
 
     data = {}
     i = 0
@@ -40,46 +33,6 @@ def express_to_flask():
 
     data = jsonify(data)
     return data
-    # return jsonify({"a":"b"})
-
-
-@api.route('/test')
-class testAPI(Resource):
-
-    # 데이터 그냥 주는 함수
-    def get(self):
-
-        notes = main.get_notes()
-        data = {}
-        i = 0
-        for n in notes:
-            tmp = {
-                'second': n[0],
-                'code': n[1]
-            }
-            data['note' + str(i)] = tmp
-            i = i + 1
-
-        data = jsonify(data)
-        return data
-
-    # 데이터를 request.json.get으로 받고 나도 return으로 주는 함수
-    def post(self):
-
-        parsed_request = request.files.get('file')
-        notes = main.post_notes(parsed_request)
-        data = {}
-        i = 0
-        for n in notes:
-            tmp = {
-                'second': n[0],
-                'code': n[1]
-            }
-            data['note' + str(i)] = tmp
-            i = i + 1
-
-        data = jsonify(data)
-        return data
 
 
 if __name__ == '__main__':
